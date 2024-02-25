230c2461bfd7e66dec8a8be45270f3f723efb788
rcchcz <rcchcz@gmail.com>
2024-02-25 20:01:29 -0300

    feat: add cliend_by_id endpoint and custom exception handlers

--- client/routers/client_router.py
+++ client/routers/client_router.py
@@ -1,10 +1,11 @@
 from typing import List
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from pydantic import BaseModel, Field
 from sqlalchemy.orm import Session
 from client.models.client_model import Client
 
 from shared.dependencies import get_db
+from shared.exceptions import NotFound
 
 router = APIRouter(prefix="/client")
 
@@ -24,6 +25,10 @@ class ClienteRequest(BaseModel):
 def list_clients(db: Session = Depends(get_db)) -> List[Client]:
     return db.query(Client).all()
 
+@router.get("/{id_client}", response_model=ClienteResponse)
+def client_by_id(id_client: int, db: Session = Depends(get_db)) -> ClienteResponse:
+    return search_client_by_id(id_client, db)
+
 @router.post("/register", response_model=ClienteResponse, status_code=201)
 def register_client(client_request: ClienteRequest, db: Session = Depends(get_db)) -> ClienteResponse:
     new_client = Client(**client_request.dict())
@@ -36,7 +41,7 @@ def register_client(client_request: ClienteRequest, db: Session = Depends(get_db
 
 @router.put("/update/{id_client}", response_model=ClienteResponse, status_code=200)
 def update_client(id_client: int, client_request: ClienteRequest, db: Session = Depends(get_db)) -> ClienteResponse:
-    client: Client = db.query(Client).get(id_client)
+    client: Client = search_client_by_id(id_client, db)
     client.name = client_request.name
     client.email = client_request.email
 
@@ -48,6 +53,15 @@ def update_client(id_client: int, client_request: ClienteRequest, db: Session = 
 
 @router.delete("/delete/{id_client}", status_code=204)
 def delete_client(id_client: int, db: Session = Depends(get_db)) -> None:
-    client = db.query(Client).get(id_client)
+    client: Client = search_client_by_id(id_client, db)
     db.delete(client)
     db.commit()
+
+# HELPER METHODS
+def search_client_by_id(id_client: int, db: Session) -> Client:
+    client: Client = db.query(Client).get(id_client)
+
+    if client is None:
+        raise NotFound('Client')
+    
+    return client
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import uvicorn
 from fastapi import FastAPI
 from client.routers import client_router
+from shared.exceptions import NotFound
+from shared.exceptions_handler import not_found_exception_handler
 # from shared.database import engine, Base
 # from client.models.client_model import Client
 
@@ -10,6 +12,7 @@ from client.routers import client_router
 app = FastAPI()
 
 app.include_router(client_router.router)
+app.add_exception_handler(NotFound, not_found_exception_handler)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=3030)
