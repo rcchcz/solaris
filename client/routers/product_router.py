90dcd20c39e48750486c5c697394821608407e14
rcchcz <rcchcz@gmail.com>
2024-02-26 13:07:34 -0300

    feat: add pagination to list endpoint

--- client/routers/product_router.py
+++ client/routers/product_router.py
@@ -1,6 +1,7 @@
 from typing import List
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Query
 from pydantic import BaseModel, Field
+from sqlalchemy import func
 from sqlalchemy.orm import Session
 from client.models.product_model import Product
 from shared.dependencies import get_db
@@ -26,9 +27,20 @@ class ProductRequest(BaseModel):
     title: str
     review_score: float
 
+# @router.get("/list", response_model=List[ProductResponse])
+# def list_products(db: Session = Depends(get_db)) -> List[Product]:
+#     return db.query(Product).all()
+
 @router.get("/list", response_model=List[ProductResponse])
-def list_products(db: Session = Depends(get_db)) -> List[Product]:
-    return db.query(Product).all()
+def list_products(page: int = Query(1, gt=0), page_size: int = Query(10, gt=0), db: Session = Depends(get_db)) -> List[Product]:
+    total_items = db.query(func.count(Product.id)).scalar()
+    total_pages = (total_items + page_size - 1)
+    if page > total_pages:
+        # TODO: move this to exceptions_handler section
+        raise HTTPException(status_code=404, detail="Page not found.")
+    skip = (page - 1) * page_size
+    products = db.query(Product).offset(skip).limit(page_size).all()
+    return products
 
 @router.get("/{id_product}", response_model=ProductResponse)
 def product_by_id(id_product: int, db: Session = Depends(get_db)) -> ProductResponse:
