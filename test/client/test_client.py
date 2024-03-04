ed95a382a187cdffa5af4c91c35076ab662bed67
rcchcz <rcchcz@gmail.com>
2024-03-04 13:18:38 -0300

    test: add tests for update endpoint

--- test/client/test_client.py
+++ test/client/test_client.py
@@ -90,6 +90,70 @@ def test_notfound_client_by_id():
         assert e.status_code == 404    
         assert e.detail == {'detail': 'Client not found.'}
 
+def test_successful_client_update():
+    # new database for each test
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
+
+    new_client = {
+        'name': 'Magalu',
+        'email': 'magalu@mail.com'
+    }
+
+    response_post = client.post('/client/register', json=new_client)
+    assert response_post.status_code == 201
+
+    update_client = {
+        'name': 'Magalu',
+        'email': 'newmagalu@mail.com',
+        'favorite_products': []
+    }
+
+    update_client_expected = update_client.copy()
+    update_client_expected['id'] = 1
+
+    response_put = client.put('/client/update/1', json=update_client)
+
+    assert response_put.status_code == 200
+    assert response_put.json()['id'] == update_client_expected['id']
+    assert response_put.json()['name'] == update_client_expected['name']
+    assert response_put.json()['email'] == update_client_expected['email']
+
+def test_fail_client_update():
+    # new database for each test
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
+
+    new_client1 = {
+        'name': 'Magalu',
+        'email': 'magalu@mail.com'
+    }
+
+    new_client2 = {
+        'name': 'Luizalabs',
+        'email': 'luizalabs@mail.com'
+    }
+
+    response_post1 = client.post('/client/register', json=new_client1)
+    assert response_post1.status_code == 201
+
+    response_post2 = client.post('/client/register', json=new_client2)
+    assert response_post2.status_code == 201
+
+    update_client = {
+        'name': 'Magalu',
+        'email': 'luizalabs@mail.com', # TESTING EMAIL ALREADY IN USE
+        'favorite_products': []
+    }
+
+    client_id = response_post1.json()['id']
+
+    try:
+        client.put(f'/client/update/{client_id}', json=update_client)
+    except HTTPException as e:
+        assert e.status_code == 409
+        assert e.detail == 'Email already in use.'
+
 # def test_list_clients():
 #     # new database for each test
 #     Base.metadata.drop_all(bind=engine)
@@ -102,29 +166,6 @@ def test_notfound_client_by_id():
 #     assert response.status_code == 200
 #     #assert response.json() == [{'id': 1, 'name': 'Kassio', 'email': 'kassio@mail.com'}, {'id': 2, 'name': 'Joao Pedro', 'email': 'jp@mail.com'}, {'id': 3, 'name': 'Gustvao', 'email': 'gustavo@mail.com'}, {'id': 4, 'name': 'Eliza', 'email': 'eliza@mail.com'}]
 #     assert response.json() == [{'id': 1, 'name': 'Kassio', 'email': 'kassio@mail.com'}, {'id': 2, 'name': 'Joao Pedro', 'email': 'jp@mail.com'}]
-
-# def test_update_client():
-#     # new database for each test
-#     Base.metadata.drop_all(bind=engine)
-#     Base.metadata.create_all(bind=engine)
-
-#     new_client = {
-#         'name': 'Magalu',
-#         'email': 'magalu@mail.com'
-#     }
-
-#     response_post = client.post('/client/register', json=new_client)
-#     id_client = response_post.json()['id']
-
-#     update_client = {
-#         'name': 'Magalu',
-#         'email': 'newmagalu@mail.com'
-#     }
-
-#     response_put = client.put(f'/client/update/{id_client}', json=update_client)
-
-#     assert response_put.status_code == 200
-#     assert response_put.json()['email'] == update_client['email']
 
 # def test_delete_client():
 #     # new database for each test
